a16e494f2a7c9f3c224dfd375317fdb49c75db92
CrHacker7 <crhacker7@gmail.com>
2026-07-14 19:24:19 +0200

    Creating POJO's

---
+++ src/main/java/com/divdev/springboot/di/app/domain/ItemInvoice.java
@@ -0,0 +1,24 @@
+package com.divdev.springboot.di.app.domain;
+
+public class ItemInvoice {
+
+	private Product product;
+	private Integer quantity;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+}
